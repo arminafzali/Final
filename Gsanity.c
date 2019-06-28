6e22c24ff15e03acee9e87701a7b033abc633f52
arminafzali <arminafzali23@gmail.com>
2019-06-28 15:42:58 +0430

    add Gsanity

---
+++ Gsanity.c
@@ -0,0 +1,39 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define LOOP_COUNT 50
+
+void printProcess(int pid){
+    int i=0;
+    for(i=0;i<LOOP_COUNT;i++){
+        printf(1,"Process %d is printing for %d time\n",pid,i);
+    }
+}
+
+void Gsanity() {
+    int pid;
+    printf(1, "Gsanity pId is:%d\n",getpid());
+    sleep(1000);
+    pid = fork();
+    if (pid == 0) {
+        nice();
+        nice();
+        printProcess(getpid());
+        printf(1, "Child %d finished\n",getpid());
+        exit();
+    } else if (pid > 0) {
+        nice();
+        nice();
+        printProcess(getpid());
+        printf(1, "Parent\n");
+    } else {
+        printf(1, "Error is fork\n");
+    }
+    printf(1, "Parent finished work\n");
+}
+
+int main(void) {
+    Gsanity();
+    exit();
+}
--- Makefile
+++ Makefile
@@ -177,6 +177,7 @@ UPROGS=\
 	_waittest\
 	_RRsanity\
 	_FRRsanity\
+	_Gsanity\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -245,7 +246,7 @@ qemu-nox-gdb: fs.img xv6.img .gdbinit
 
 EXTRA=\
 	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c\
-	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c testppid.c waittest.c RRsanity.c FRRsanity.c\
+	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c testppid.c waittest.c RRsanity.c FRRsanity.c Gsanity.c\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
 	.gdbinit.tmpl gdbutil\
